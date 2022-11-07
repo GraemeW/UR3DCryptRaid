b93db036f2715a06541a03561c908e77c7d5e100
GraemeMW <graememwilliams@gmail.com>
2022-11-06 17:47:49 -0800

    More boilerplate for grabbing physical objects

--- Source/UR3DCryptRaid/Grabber.cpp
+++ Source/UR3DCryptRaid/Grabber.cpp
@@ -4,14 +4,9 @@
 #include "Grabber.h"
 #include "Engine/World.h"
 
-// Sets default values for this component's properties
 UGrabber::UGrabber()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -20,20 +15,71 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (PhysicsHandle != nullptr)
+	{
+		FString PhysicsHandleName = PhysicsHandle->GetName();
+		UE_LOG(LogTemp, Display, TEXT("Physics Handle Name: %s"), *PhysicsHandleName);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Physics Handle could not be found!  Grab functionality will not work!"));
+	}
 }
 
-
-// Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
 
-	AActor* Owner = GetOwner();
-	FVector StartPoint = Owner->GetActorLocation();
-
-	FVector EndPoint = GetComponentLocation() + GetForwardVector() * MaxGrabDistance;
+void UGrabber::Grab()
+{
+	UE_LOG(LogTemp, Display, TEXT("Initiated grabber"));
+	FVector StartPoint;
+	FVector EndPoint;
+	GetGrabPoints(StartPoint, EndPoint);
 	DrawDebugLine(GetWorld(), StartPoint, EndPoint, FColor::Red, false, 0.1);
+	DrawDebugSphere(GetWorld(), EndPoint, 10, 10, FColor::Blue, false, 5);
+	CastToGrab(StartPoint, EndPoint);
+}
+
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Display, TEXT("Released grabber"));
+}
+
+void UGrabber::GetGrabPoints(FVector& OutStartPoint, FVector& OutEndPoint)
+{
+	AActor* Owner = GetOwner();
+	OutStartPoint = Owner->GetActorLocation();
+	OutEndPoint = GetComponentLocation() + GetForwardVector() * MaxGrabDistance;
+}
+
+void UGrabber::CastToGrab(const FVector& StartPoint, const FVector& EndPoint)
+{
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
+	FHitResult HitResult;
+
+	bool HasHit = false;
+	HasHit = GetWorld()->SweepSingleByChannel(HitResult,
+		StartPoint, EndPoint,
+		FQuat::Identity,
+		ECC_GameTraceChannel2,
+		Sphere
+	);
+
+	if (HasHit)
+	{
+		AActor* HitActor = HitResult.GetActor();
+		FString HitActorName = HitActor->GetActorNameOrLabel();
+
+		DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10, 10, FColor::Green, false, 5);
+		UE_LOG(LogTemp, Display, TEXT("Found a grabbable object:  %s"), *HitActorName);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Display, TEXT("Nothing to grab"));
+	}
 }
 
--- Source/UR3DCryptRaid/Grabber.h
+++ Source/UR3DCryptRaid/Grabber.h
@@ -4,8 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/SceneComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabber.generated.h"
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class UR3DCRYPTRAID_API UGrabber : public USceneComponent
@@ -24,6 +24,20 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	// Blueprint Functions
+	UFUNCTION(BlueprintCallable) void Grab();
+	UFUNCTION(BlueprintCallable) void Release();
+
+
 private:
+	// Exposed Properties
 	UPROPERTY(EditAnywhere) float MaxGrabDistance = 400;
+	UPROPERTY(EditAnywhere) float GrabRadius = 100;
+
+	// Cached References
+	UPhysicsHandleComponent *PhysicsHandle = nullptr;
+
+	// Private Methods
+	void GetGrabPoints(FVector& StartPoint, FVector& EndPoint);
+	void CastToGrab(const FVector& StartPoint, const FVector& EndPoint);
 };
